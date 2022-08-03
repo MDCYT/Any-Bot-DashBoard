55b102aafb81c13b69b53ef140729218ffe6ddcb
MDCDEV <me@mdcdev.me>
2022-08-03 18:36:04 -0500

    Update invite.js

--- src/config/invite.js
+++ src/config/invite.js
@@ -1,5 +1,6 @@
 module.exports = {
     clientId: process.env.DISCORD_BOT_ID,
     scopes: ["bot", "applications.commands", "identify", "email", "guilds", "guilds.join"],
-    permissions: '8'
+    permissions: '8',
+    redirectUri: process.env.DASHBOARD_DOMAIN + '/discord/callback',
 }
